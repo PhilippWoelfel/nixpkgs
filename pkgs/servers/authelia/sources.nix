79bbc2b5301d00064f38a578d5ac2de8dcf54fcd
Philipp Woelfel <philipp.woelfel@gmail.com>
2024-04-02 10:11:39 -0600

    authelia 4.38.7 via update.sh

--- pkgs/servers/authelia/sources.nix
+++ pkgs/servers/authelia/sources.nix
@@ -1,14 +1,14 @@
 { fetchFromGitHub }:
 rec {
   pname = "authelia";
-  version = "4.37.5";
+  version = "4.38.7";
 
   src = fetchFromGitHub {
     owner = "authelia";
     repo = "authelia";
     rev = "v${version}";
-    hash = "sha256-xsdBnyPHFIimhp2rcudWqvVR36WN4vBXbxRmvgqMcDw=";
+    hash = "sha256-VbAF2uIFL1jN469KGpnkm5erwryLoatZukZgiea+RQA=";
   };
-  vendorHash = "sha256-mzGE/T/2TT4+7uc2axTqG3aeLMnt1r9Ya7Zj2jIkw/w=";
-  npmDepsHash = "sha256-MGs6UAxT5QZd8S3AO75mxuCb6U0UdRkGEjenOVj+Oqs=";
+  vendorHash = "sha256-gyBCpNH85LwPLpENZPMuk98YoFsNWbudsw59W35cUKY=";
+  npmDepsHash = "sha256-UfkvjkmjXM8SzpSvHY7fkGP3qCRUDgF8WJUAlS2c1WM=";
 }
